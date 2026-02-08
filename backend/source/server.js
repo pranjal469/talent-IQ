c38db9fde812d8ea7c505ff3da09799dadde41fe
pranjal_katoch <pranjalkatoch710@gmail.com>
2026-02-08 19:06:58 +0530

    initial commit

---
+++ backend/source/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+const app = express();
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "Server is running" });
+});
+
+app.listen(ENV.PORT, () => {
+    console.log("Server is running on port 5000");
+});
